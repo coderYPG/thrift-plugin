2eb3c4f363ab7fbc1ce3309e49eaa539d6d732d9
GitHub Action <action@github.com>
2022-09-06 15:09:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/coderypg/thriftplugin/MyBundle.kt
+++ src/main/kotlin/com/github/coderypg/thriftplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.coderypg.thriftplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/coderypg/thriftplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/coderypg/thriftplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.coderypg.thriftplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.coderypg.thriftplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
